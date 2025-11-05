de69dacc846336a134d071feabdb62e3619466c9
Fabian Guera <fabian@odyssey.systems>
2025-11-05 17:09:54 +0000

    build just x86.

--- build_manual.sh
+++ build_manual.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 IMAGE="gueraf/self_hosted_cuda_runner"
 TAG="${1:-latest}"
 
-echo "Building and pushing multi-arch image $IMAGE:$TAG for platforms linux/amd64,linux/arm64"
-docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE:$TAG" --push .
+# echo "Building and pushing multi-arch image $IMAGE:$TAG for platforms linux/amd64,linux/arm64"
+# docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE:$TAG" --push .
+docker buildx build -t "$IMAGE:$TAG" --push .
 
 echo "Done."
