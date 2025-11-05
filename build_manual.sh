f34c410f862151bc8dbfc9dc9ef95827200e359d
Fabian Guera <fabian@odyssey.systems>
2025-11-05 19:55:06 +0000

    Make arm work.

--- build_manual.sh
+++ build_manual.sh
@@ -5,7 +5,6 @@ IMAGE="gueraf/self_hosted_cuda_runner"
 TAG="${1:-arm}"
 
 # echo "Building and pushing multi-arch image $IMAGE:$TAG for platforms linux/amd64,linux/arm64"
-# docker buildx build --platform linux/amd64,linux/arm64 -t "$IMAGE:$TAG" --push .
-docker buildx build -t "$IMAGE:$TAG" --push .
+docker buildx build --platform linux/arm64 -t "$IMAGE:$TAG" --push .
 
 echo "Done."
